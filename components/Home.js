0dc968fb1d69d663563a6ff44f1e9ccd025cf15c
Michell Sosa <michellsosa89@gmail.com>
2021-08-15 19:44:42 -0600

    Adding style and header to homepage

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Header from './Header.js';
+
+const Home = () => {
+return (
+
+    <Header/>
+);
+
+}
+
+export default Home;
